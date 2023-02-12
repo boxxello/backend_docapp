c1618ca2155885ab07615b69c154db508276bf85
fbosso <f.bosso4@studenti.unisa.it>
2023-02-12 23:58:58 +0100

    Aggiunta dei dao al backend invece di shared

--- shared_docapp/src/main/java/com/docapp/shared_docapp/dao_related/ResultSetExtractor.java
+++
@@ -1,8 +0,0 @@
-package com.docapp.shared_docapp.dao_related;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface  ResultSetExtractor<B> {
-    B extract (ResultSet rs) throws SQLException;
-}
