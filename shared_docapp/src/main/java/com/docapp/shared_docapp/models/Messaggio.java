25286d8a2517a90ccf13a67f632ea3acb5a1be23
fbosso <f.bosso4@studenti.unisa.it>
2023-02-17 10:40:58 +0100

    Fix shared model (probabilmente verrà usato solo per la mobileapp)

--- shared_docapp/src/main/java/com/docapp/shared_docapp/models/Messaggio.java
+++ shared_docapp/src/main/java/com/docapp/shared_docapp/models/Messaggio.java
@@ -4,33 +4,19 @@ import java.util.HashMap;
 
 public class Messaggio implements IEntity {
     public static final String TABLE_NAME ="Messaggio";
-    private String  email_studente, testo, tms;
-    private int id_conversazione, id_messaggio;
+    private String  testo, tms;
+    private long id_conversazione, id_messaggio, id_studente;
 
     public Messaggio(){}
 
-    public Messaggio(int id_conversazione, String email_studente, String testo, String tms){
-        this.email_studente = email_studente;
+    public Messaggio(long id_conversazione, long id_studente, String testo, String tms){
+        this.id_studente = id_studente;
         this.id_conversazione = id_conversazione;
         this.testo = testo;
         this.tms = tms;
     }
 
-    public void setId_messaggio(int id_messaggio){
-        this.id_messaggio = id_messaggio;
-    }
 
-    public int getId_messaggio(){
-        return this.id_messaggio;
-    }
-
-    public String getTms(){
-        return this.tms;
-    }
-
-    public void setTms(String tms){
-        this.tms = tms;
-    }
 
     public String getTesto() {
         return testo;
@@ -40,30 +26,46 @@ public class Messaggio implements IEntity {
         this.testo = testo;
     }
 
-    public int getId_conversazione() {
+    public String getTms() {
+        return tms;
+    }
+
+    public void setTms(String tms) {
+        this.tms = tms;
+    }
+
+    public long getId_conversazione() {
         return id_conversazione;
     }
 
-    public void setId_conversazione(int id_conversazione) {
+    public void setId_conversazione(long id_conversazione) {
         this.id_conversazione = id_conversazione;
     }
 
-    public String getEmail_studente() {
-        return email_studente;
+    public long getId_messaggio() {
+        return id_messaggio;
     }
 
-    public void setEmail_studente(String email_studente) {
-        this.email_studente = email_studente;
+    public void setId_messaggio(long id_messaggio) {
+        this.id_messaggio = id_messaggio;
+    }
+
+    public long getId_studente() {
+        return id_studente;
+    }
+
+    public void setId_studente(long id_studente) {
+        this.id_studente = id_studente;
     }
 
     @Override
     public HashMap<String, ?> toHashMap() {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("id_messaggio", id_messaggio);
-        map.put("id_conversazione", id_conversazione);
-        map.put("email_studente", email_studente);
+        map.put("id", id_messaggio);
+        map.put("conversazione_id", id_conversazione);
+        map.put("id_studente", id_studente);
         map.put("testo", testo);
-        map.put("tms", tms);
+        map.put("timestamp_msg", tms);
         return map;
     }
 }
