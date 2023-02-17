09a8b7835fd38485ae439959c8b1a5324339541a
fbosso <f.bosso4@studenti.unisa.it>
2023-02-17 10:40:37 +0100

    Fix shared model (probabilmente verrà usato solo per la mobileapp)

--- shared_docapp/src/main/java/com/docapp/shared_docapp/models/Conversazione.java
+++ shared_docapp/src/main/java/com/docapp/shared_docapp/models/Conversazione.java
@@ -4,30 +4,18 @@ import java.util.HashMap;
 
 public class Conversazione implements IEntity {
     public static final String TABLE_NAME ="Conversazione";
-    private String nome_conversazione,email_studente;
-    private int id_conversazione ;
+    private String nome_conversazione,
+     studente1,studente2;
+
+    private String id_conversazione ;
 
     public Conversazione(){}
 
-    public Conversazione(String nome_conversazione, String email_studente){
-        this.email_studente = email_studente;
+    public Conversazione(String nome_conversazione, String studente1, String studente2, String id_conversazione) {
         this.nome_conversazione = nome_conversazione;
-    }
-
-    public int getId_conversazione() {
-        return id_conversazione;
-    }
-
-    public void setId_conversazione(int id_conversazione) {
+        this.studente1 = studente1;
+        this.studente2 = studente2;
         this.id_conversazione = id_conversazione;
-    }
-
-    public String getEmail_studente() {
-        return email_studente;
-    }
-
-    public void setEmail_studente(String email_studente) {
-        this.email_studente = email_studente;
     }
 
     public String getNome_conversazione() {
@@ -38,11 +26,36 @@ public class Conversazione implements IEntity {
         this.nome_conversazione = nome_conversazione;
     }
 
+    public String getStudente1() {
+        return studente1;
+    }
+
+    public void setStudente1(String studente1) {
+        this.studente1 = studente1;
+    }
+
+    public String getStudente2() {
+        return studente2;
+    }
+
+    public void setStudente2(String studente2) {
+        this.studente2 = studente2;
+    }
+
+    public String getId_conversazione() {
+        return id_conversazione;
+    }
+
+    public void setId_conversazione(String id_conversazione) {
+        this.id_conversazione = id_conversazione;
+    }
+
     @Override
     public HashMap<String, ?> toHashMap() {
         HashMap<String, Object> map = new HashMap<>();
-        map.put("id_conversazione", id_conversazione);
-        map.put("email_studente", email_studente);
+        map.put("id", id_conversazione);
+        map.put("studente1", studente1);
+        map.put("studente2", studente2);
         map.put("nome_conversazione", nome_conversazione);
         return map;
     }
