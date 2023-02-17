87379aa23072d321228dd0a3d63c8d2a933ea0a5
fbosso <f.bosso4@studenti.unisa.it>
2023-02-17 09:34:38 +0100

    Aggiunto controller per i post (put, del)

--- backend/src/main/java/com/docapp/springjwt/controllers/PostController.java
+++ backend/src/main/java/com/docapp/springjwt/controllers/PostController.java
@@ -1,44 +1,82 @@
 package com.docapp.springjwt.controllers;
 
-import com.docapp.DAO.PostDAO;
-import com.docapp.Utils.ConnPom;
-import com.docapp.shared_docapp.models.Post;
+
+
+import com.docapp.springjwt.exceptions.ResourceNotFoundException;
+import com.docapp.springjwt.models.Post;
+import com.docapp.springjwt.repository.PostRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/api/post")
 public class PostController {
-    @GetMapping("/all")
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public ResponseEntity<String> allAccess() {
-        PostDAO dao = new PostDAO(ConnPom.getDatasource());
-        try {
-            List<Post> posts = dao.doRetrieveAll();
-            List<HashMap<String, ?>> list_of_hashmaps = new ArrayList<>();
-            for (Post post : posts) {
-                list_of_hashmaps.add(post.toHashMap());
-            }
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(list_of_hashmaps);
-            return ResponseEntity.ok().body(json);
-        } catch (SQLException | JsonProcessingException throwables) {
-            return null;
-        }
+
+    @Autowired
+    private PostRepository postRepository;
+
+    // Get all posts
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
     }
+
+    // Get a single post by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable(value = "id") Long postId)
+            throws ResourceNotFoundException {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found for this id :: " + postId));
+        return ResponseEntity.ok().body(post);
+    }
+
+    // Create a new post
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    // Update an existing post
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable(value = "id") Long postId,
+                                           @RequestBody Post postDetails) throws ResourceNotFoundException {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found for this id :: " + postId));
+
+        post.setTitolo(postDetails.getTitolo());
+        post.setTesto(postDetails.getTesto());
+        post.setDomanda(postDetails.isDomanda());
+
+        final Post updatedPost = postRepository.save(post);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    // Delete a post
+    @DeleteMapping("/{id}")
+    public Map<String, Boolean> deletePost(@PathVariable(value = "id") Long postId)
+            throws ResourceNotFoundException {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found for this id :: " + postId));
+
+        postRepository.delete(post);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
 }
