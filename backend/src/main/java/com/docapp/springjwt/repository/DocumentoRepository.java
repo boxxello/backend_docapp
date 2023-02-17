295ebc46ed00147a62d39ba9d9f7dd8a64bb8d8f
fbosso <f.bosso4@studenti.unisa.it>
2023-02-17 22:04:44 +0100

    Fix aggiunta decoratore @Repository

--- backend/src/main/java/com/docapp/springjwt/repository/DocumentoRepository.java
+++ backend/src/main/java/com/docapp/springjwt/repository/DocumentoRepository.java
@@ -4,9 +4,10 @@ import com.docapp.springjwt.models.Conversazione;
 import com.docapp.springjwt.models.Documento;
 import com.docapp.springjwt.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
 
 
