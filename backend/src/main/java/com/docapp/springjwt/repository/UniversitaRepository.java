d27844d433060f06ed3c84e5de602a1077a2850e
fbosso <f.bosso4@studenti.unisa.it>
2023-02-23 11:00:11 +0100

    Aggiunte interfacce alla UniversitaRepository

--- backend/src/main/java/com/docapp/springjwt/repository/UniversitaRepository.java
+++ backend/src/main/java/com/docapp/springjwt/repository/UniversitaRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UniversitaRepository extends JpaRepository<Universita, Long> {
 
-    List<Universita> findAllByNome(String nome);
+    Optional<List<Universita>> findAllByNome(String nome);
+
+    Optional<Universita> findByNome(String nome);
+
+    Optional<List<Universita>> findAllByNomeLike(String nome);
 }
 
