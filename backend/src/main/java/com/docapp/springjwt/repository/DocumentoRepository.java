e0ba46f6f262dacfd5ff5c2960a864ca0a648e4a
fbosso <f.bosso4@studenti.unisa.it>
2023-02-23 08:34:56 +0100

    Aggiunta interfaccia per trovare il documento in base all'id e allo studente

--- backend/src/main/java/com/docapp/springjwt/repository/DocumentoRepository.java
+++ backend/src/main/java/com/docapp/springjwt/repository/DocumentoRepository.java
@@ -7,11 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
 
 
-
     List<Documento> findAllByStudenteId(Long userId);
+
     List<Documento> findAllByStudente(User user);
+
+    Optional<Documento> findByIdAndStudente(Long id, User currentUser);
 }
